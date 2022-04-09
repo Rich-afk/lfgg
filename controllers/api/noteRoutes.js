36c1cf75fe6483fb1110c52b3c18f14c9155a5cb
Sean Duiser <duzer84@gmail.com>
2022-04-08 21:56:17 -0400

    need to get routing all set up. Languages need to display notes. Then, when a note is clicked, the details need to show on a new page

--- controllers/api/noteRoutes.js
+++ controllers/api/noteRoutes.js
@@ -2,6 +2,40 @@ const router = require('express').Router();
 const { Note } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.get('/', async (req, res) => {
+  try {
+    const LanguagePostData = await Note.findAll({
+      include: [
+        title,
+        content,
+        // {
+        //   model: User,
+        //   attributes: ['name'],
+        // },
+        // {
+        //   model: Comment,
+        //   include: {
+        //     model: User,
+        //     attributes: ['name'],
+        //   }
+        // },
+      ],
+    });
+
+    const posts = LanguagePostData.map((langPost) =>
+      langPost.get({ plain: true })
+    );
+
+    res.render('notes', {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 router.post('/', withAuth, async (req, res) => {
   try {
     const newNotePost = await Note.create({
